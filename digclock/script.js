09ac8dfe67eff826923fadb6721d66d9e41948b8
samritbasnet <hereiamsamrit7@outlook.com>
2024-03-06 14:18:59 -0500

    change in styling and js code from traversry clone

--- digclock/script.js
+++ digclock/script.js
@@ -1,48 +1,37 @@
-const hourEl = document.querySelector('.hour')
-const minuteEl = document.querySelector('.minute')
-const secondEl = document.querySelector('.second')
-const timeEl = document.querySelector('.time')
-const dateEl = document.querySelector('.date')
-const toggle = document.querySelector('.toggle')
+const hourEl = document.querySelector('.hour');
+const minuteEl = document.querySelector('.minute');
+const secondEl = document.querySelector('.second');
+const timeEl = document.querySelector('.time');
+const dateEl = document.querySelector('.date');
+const toggle = document.querySelector('.toggle');
 
 const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
 
 toggle.addEventListener('click', (e) => {
-    const html = document.querySelector('html')
-    if (html.classList.contains('dark')) {
-        html.classList.remove('dark')
-        e.target.innerHTML = 'Dark mode'
-    } else {
-        html.classList.add('dark')
-        e.target.innerHTML = 'Light mode'
-    }
-})
+    const html = document.querySelector('html');
+    html.classList.toggle('dark');
+    e.target.textContent = html.classList.contains('dark') ? 'Light mode' : 'Dark mode';
+});
 
-function setTime() {
-    const time = new Date();
-    const month = time.getMonth()
-    const day = time.getDay()
-    const date = time.getDate()
-    const hours = time.getHours()
-    const hoursForClock = hours >= 13 ? hours % 12 : hours;
-    const minutes = time.getMinutes()
-    const seconds = time.getSeconds()
-    const ampm = hours >= 12 ? 'PM' : 'AM'
+function updateTime() {
+    const now = new Date();
+    const hours = now.getHours();
+    const minutes = now.getMinutes();
+    const seconds = now.getSeconds();
+    const month = months[now.getMonth()];
+    const day = days[now.getDay()];
+    const date = now.getDate();
+    const ampm = hours >= 12 ? 'PM' : 'AM';
+    const hoursForClock = hours % 12 || 12;
 
-    hourEl.style.transform = `translate(-50%, -100%) rotate(${scale(hoursForClock, 0, 12, 0, 360)}deg)`
-    minuteEl.style.transform = `translate(-50%, -100%) rotate(${scale(minutes, 0, 60, 0, 360)}deg)`
-    secondEl.style.transform = `translate(-50%, -100%) rotate(${scale(seconds, 0, 60, 0, 360)}deg)`
+    hourEl.style.transform = `translate(-50%, -100%) rotate(${(hoursForClock * 30) + (minutes / 2)}deg)`;
+    minuteEl.style.transform = `translate(-50%, -100%) rotate(${minutes * 6}deg)`;
+    secondEl.style.transform = `translate(-50%, -100%) rotate(${seconds * 6}deg)`;
 
-    timeEl.innerHTML = `${hoursForClock}:${minutes < 10 ? `0${minutes}` : minutes} ${ampm}`
-    dateEl.innerHTML = `${days[day]}, ${months[month]} <span class="circle">${date}</span>`
+    timeEl.textContent = `${hoursForClock}:${minutes < 10 ? '0' + minutes : minutes} ${ampm}`;
+    dateEl.innerHTML = `${day}, ${month} <span class="circle">${date}</span>`;
 }
 
-// StackOverflow https://stackoverflow.com/questions/10756313/javascript-jquery-map-a-range-of-numbers-to-another-range-of-numbers
-const scale = (num, in_min, in_max, out_min, out_max) => {
-    return (num - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
-}
-
-setTime()
-
-setInterval(setTime, 1000)
+updateTime();
+setInterval(updateTime, 1000);
